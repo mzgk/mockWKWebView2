a3901cff3ac333e102dcc3906f681f39a1d6d16b
takeshi mizugaki <takeshi.mizugaki@ast-net.co.jp>
2016-10-31 14:56:34 +0900

    Initial Commit

---
+++ mockWKWebView2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  mockWKWebView2
+//
+//  Created by mzgk on 2016/10/31.
+//  Copyright © 2016年 mzgk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
